7a89289e3a20157c4696dacb82031087a82bb732
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2024-01-17 00:21:01 +0600

    Navbar responsive done

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt corporis
+          doloribus eos cumque non nemo reiciendis laborum minima culpa,
+          consectetur harum similique libero! Corporis exercitationem voluptatem
+          optio ab quas, in quibusdam perspiciatis laboriosam harum minus odit
+          culpa temporibus suscipit modi ea doloribus pariatur reprehenderit
+          veritatis. Nisi quibusdam error saepe quos, iste quia quidem Lorem
+          ipsum dolor sit amet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          dolores eveniet vero perferendis voluptas facilis beatae laborum
+          quibusdam. Nemo eaque iure iste voluptates! Nesciunt quos hic corrupti
+          eos praesentium dolorem!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
