11339e24ffb21ed2b2e34dcbe5a079fb43f63056
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2024-04-01 02:30:53 +0600

    Navbar design, Add Product and Sidebar Create

---
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./AddProduct.css";
+
+const AddProduct = () => {
+  return <div className="add-product">AddProduct</div>;
+};
+
+export default AddProduct;
--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,7 +1,13 @@
+import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 import "./Admin.css";
 
 const Admin = () => {
-  return <div className="admin"></div>;
+  return (
+    <div className="admin">
+      <Sidebar />
+    </div>
+  );
 };
 
 export default Admin;
